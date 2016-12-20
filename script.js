f5b91dedf2eaf4b8020ee895d16c01c1fad52b6a
dargaCode <darga.code@gmail.com>
2016-12-19 21:25:47 -0800

    Add canvas with click event to draw square

---
+++ script.js
@@ -0,0 +1,23 @@
+// use strict;
+
+const SQUARE_WIDTH = 70;
+const MIDPOINT = SQUARE_WIDTH / 2;
+
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = '#D00';
+ctx.fillRect(20, 20, 70, 70);
+
+canvas.addEventListener('click', drawSquare);
+
+function drawSquare(e) {
+  const clickX = e.offsetX;
+  const clickY = e.offsetY;
+  const squareX = clickX - MIDPOINT;
+  const squareY = clickY - MIDPOINT;
+
+  console.log(squareX, squareY);
+
+  ctx.fillRect(squareX, squareY, 70, 70);
+}
